15c941aeeb1bbc4d24593e9a6b819ef4b3fc746d
jayHsia <jay.hsia@mobile.com.tw>
2023-03-13 11:57:14 +0800

    1. add mp4

---
+++ CalendarSelectRange/Extension/CGFloat+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  CGFloat+Extension.swift
+//  CalendarSelectRange
+//
+//  Created by mtaxi on 2023/3/8.
+//
+
+import Foundation
+
+extension CGFloat{
+    static let cornerRadius:CGFloat = 10
+    
+    static let smallFontSize:CGFloat = 15
+    static let regularFontSize:CGFloat = 17
+    static let largeFontSize:CGFloat = 20
+    static let hugeFontSize:CGFloat = 25
+    
+    static let spacing:CGFloat = 16
+    static let smallSpacing:CGFloat = 8
+    
+    static let buttonSize:CGFloat = 50
+    
+}
+
